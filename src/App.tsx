a4114121ade4ce4c680fdcb48e717e0a2d84c9f9
Diana <diana.dev777@gmail.com>
2024-02-06 20:41:43 +0600

    feat: add apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { ApolloProvider } from '@apollo/client';
 import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
 import router from './components/Routes';
+import client from './constants/apollo-client';
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });
 const App = () => (
-  <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
-    <Container>
-      <RouterProvider router={router} />
-    </Container>
-  </ThemeProvider>
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <RouterProvider router={router} />
+      </Container>
+    </ThemeProvider>
+  </ApolloProvider>
 );
 
 export default App;
