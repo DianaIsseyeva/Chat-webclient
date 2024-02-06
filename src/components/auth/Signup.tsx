af738a8bbdbb111ad3fed36b24f15b8226bcbe82
Diana <diana.dev777@gmail.com>
2024-02-06 21:23:39 +0600

    feat: implement create user

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,12 +1,28 @@
 import { Link as MUILink } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useCreateUser } from '../../hooks/useCreateUser';
 import Auth from './Auth';
-const Signup = () => (
-  <Auth submitLabel='Signup' onSubmit={async () => {}}>
-    <Link to={'login'} style={{ alignSelf: 'center' }}>
-      <MUILink>Login</MUILink>
-    </Link>
-  </Auth>
-);
+const Signup = () => {
+  const [createUser] = useCreateUser();
+  return (
+    <Auth
+      submitLabel='Signup'
+      onSubmit={async ({ email, password }) => {
+        await createUser({
+          variables: {
+            createUserInput: {
+              email,
+              password,
+            },
+          },
+        });
+      }}
+    >
+      <Link to={'login'} style={{ alignSelf: 'center' }}>
+        <MUILink>Login</MUILink>
+      </Link>
+    </Auth>
+  );
+};
 
 export default Signup;
