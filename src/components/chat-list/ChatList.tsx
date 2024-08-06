6796c0fbcc9dcb45234974ec1350cbb929ee512f
Diana <diana.dev777@gmail.com>
2024-08-06 16:29:41 +0300

    feat: implement add chat modal

--- src/components/chat-list/ChatList.tsx
+++ src/components/chat-list/ChatList.tsx
@@ -1,17 +1,23 @@
 import { Divider, Stack } from '@mui/material';
 import List from '@mui/material/List';
+import { useState } from 'react';
+import ChatListAdd from './chat-list-add/ChatListAdd';
 import ChatListHeader from './chat-list-header/ChatListHeader';
 import ChatListItem from './chat-list-item/ChatListItem';
 
 const ChatList = () => {
+  const [chatListAddVisible, setChatListAddVisible] = useState(false);
   return (
-    <Stack>
-      <ChatListHeader />
-      <Divider />
-      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', maxHeight: '90vh', overflow: 'auto' }}>
-        <ChatListItem />
-      </List>
-    </Stack>
+    <>
+      <ChatListAdd open={chatListAddVisible} handleClose={() => setChatListAddVisible(false)} />
+      <Stack>
+        <ChatListHeader handleAddChat={() => setChatListAddVisible(true)} />
+        <Divider />
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', maxHeight: '90vh', overflow: 'auto' }}>
+          <ChatListItem />
+        </List>
+      </Stack>
+    </>
   );
 };
 export default ChatList;
